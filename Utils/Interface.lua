36a6e1aa9158f8c336f10551a850d9aa0bfce6b3
Daniel <96255550+sawedoffbilly@users.noreply.github.com>
2026-08-18 19:27:22 -0700

    Update Interface.lua

--- Utils/Interface.lua
+++ Utils/Interface.lua
@@ -1,76 +1,174 @@
+local Players = game:GetService("Players")
+
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local Humanoid = Character:WaitForChild("Humanoid")
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+
 local Utils = {}
 
-Utils.Init = function()
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/sawedoffbilly/saiseiclient/refs/heads/main/Utils/Interface.lua'))()
+local Base = "https://raw.githubusercontent.com/sawedoffbilly/saiseiclient/refs/heads/main/"
 
-	local SaiseiTab = library:AddTab("Saisei");
-	local DebugTab = library:AddTab("Debug");
-	local SettingsTab = library:AddTab("Settings");
+local function HttpRequire(Folder)
+	local FileURL = Base .. Folder
+	local FileContent = game:HttpGet(FileURL)
 
-	local SaiseiColumn1 = SaiseiTab:AddColumn();
-	local SaiseiCombat = SaiseiColumn1:AddSection("Combat")
-
-	SaiseiCombat:AddDivider("Main Cheats");
-	SaiseiCombat:AddToggle{text = "Autoparry: Disabled", flag = "AutoParryEnabled"}
-	SaiseiCombat:AddSlider{text = "Autoparry Delay", flag = "AutoparryDelay", min = 0, max = 1, value = 0.4, suffix = "s"}
-
-	SaiseiCombat:AddDivider("Rage Cheats");
-	SaiseiCombat:AddToggle{text = "Cooldowns: Disabled", flag = "CooldownsEnabled"}
-	SaiseiCombat:AddToggle{text = "Infinite Stamina: Disabled", flag = "InfiniteStaminaEnabled"}
-	SaiseiCombat:AddToggle{text = "Stun: Enabled", flag = "StunEnabled"}
-
-	local SettingsColumn = SettingsTab:AddColumn(); 
-	local SettingsColumn2 = SettingsTab:AddColumn(); 
-
-	local SettingSection = SettingsColumn:AddSection("Menu"); 
-
-	local Warning = library:AddWarning({type = "confirm"});
-
-	SettingSection:AddBind({text = "Open / Close", flag = "UI Toggle", nomouse = true, key = "End", callback = function()
-		library:Close();
-	end});
-
-	SettingSection:AddColor({text = "Accent Color", flag = "Menu Accent Color", color = Color3.new(0.599623620510101318359375, 0.447115242481231689453125, 0.97174417972564697265625), callback = function(color)
-		if library.currentTab then
-			library.currentTab.button.TextColor3 = color;
-		end
-
-		for i,v in pairs(library.theme) do
-			v[(v.ClassName == "TextLabel" and "TextColor3") or (v.ClassName == "ImageLabel" and "ImageColor3") or "BackgroundColor3"] = color;
-		end
-	end});
-
-	local backgroundlist = {
-		Floral = "rbxassetid://5553946656",
-		Flowers = "rbxassetid://6071575925",
-		Circles = "rbxassetid://6071579801",
-		Hearts = "rbxassetid://6073763717"
-	};
-
-	local back = SettingSection:AddList({text = "Background", max = 4, flag = "background", values = {"Floral", "Flowers", "Circles", "Hearts"}, value = "Floral", callback = function(v)
-		if library.main then
-			library.main.Image = backgroundlist[v];
-		end
-	end});
-
-	back:AddColor({flag = "backgroundcolor", color = Color3.new(), callback = function(color)
-		if library.main then
-			library.main.ImageColor3 = color;
-		end
-	end, Trans = 1, calltrans = function(Trans)
-		if library.main then
-			library.main.ImageTransparency = 1 - Trans;
-		end
-	end});	
-
-	SettingSection:AddSlider({text = "Tile Size", min = 50, max = 500, value = 50, callback = function(size)
-		if library.main then
-			library.main.TileSize = UDim2.new(0, size, 0, size);
-		end
-	end});
-
-	library:Init();
-	library:selectTab(library.tabs[1]);
+	return loadstring(FileContent)()
 end
+
+local ESPSettings = {
+	BoxColor = Color3.fromRGB(255, 0, 0),
+	TracerColor = Color3.fromRGB(255, 0, 0),
+	HealthEndColor = Color3.fromRGB(255, 0, 0),
+	HealthStartColor = Color3.fromRGB(71, 255, 51),
+	TracerThickness = 1,
+	BoxThickness = 1,
+	Tracers = true,
+	Distance = true,
+	Username = true,
+	StaminaBar = true,
+	PostureBar = true
+}
+
+Utils.Init = function()
+	local Repository = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+
+	local Library = loadstring(game:HttpGet(Repository .. 'Library.lua'))()
+	local ThemeManager = loadstring(game:HttpGet(Repository .. 'addons/ThemeManager.lua'))()
+	local SaveManager = loadstring(game:HttpGet(Repository .. 'addons/SaveManager.lua'))()
+	
+	local ESP = HttpRequire("Libs/ESP.lua")
+	
+	local Connections = {}
+
+	local Window = Library:CreateWindow({
+		Title = "Natrix Hub",
+		Center = true,
+		AutoShow = true,
+		TabPadding = 50,
+		MenuFadeTime = 0.2
+	})
+
+	local Tabs = {
+		Saisei = Window:AddTab('Saisei'),
+		Debug = Window:AddTab('Debug'),
+		Settings = Window:AddTab('Settings'),
+	}
+
+	local CombatGroupBox = Tabs.Saisei:AddLeftGroupbox('Combat')
+	local MenuGroupBox = Tabs.Settings:AddLeftGroupbox('Menu')
+	
+	CombatGroupBox:AddToggle('AutoParry', {
+		Text = 'AutoParry',
+		Default = false, 
+		Tooltip = 'Whether or not AutoParry is enabled for combat.',
+
+		Callback = function(Value)
+			print('[cb] MyToggle changed to:', Value)
+		end
+	})
+	
+	CombatGroupBox:AddToggle('ESP', {
+		Text = 'ESP',
+		Default = false, 
+		Tooltip = 'Whether or not ESP is enabled for combat.',
+
+		Callback = function(Value)
+			if Value == true then
+				ESP.Start()
+				
+				for _, Player in pairs(Players:GetPlayers()) do
+					if Player ~= nil and Player.Parent ~= nil and Player ~= LocalPlayer then
+						ESP.CreateESP(Player, ESPSettings)
+					end
+				end
+				
+				Connections["ESP_PlayerAdded"] = Players.PlayerAdded:Connect(ESP.CreateESP)
+				Connections["ESP_PlayerRemoving"] = Players.PlayerRemoving:Connect(ESP.RemoveESP)
+			else
+				for _, Player in pairs(Players:GetPlayers()) do
+					if Player ~= nil and Player.Parent ~= nil and Player ~= LocalPlayer then
+						ESP.RemoveESP(Player)
+					end
+				end
+				
+				if Connections["ESP_PlayerAdded"] then
+					Connections["ESP_PlayerAdded"]:Disconnect()
+					Connections["ESP_PlayerAdded"] = nil
+				end
+				
+				if Connections["ESP_PlayerRemoving"] then
+					Connections["ESP_PlayerRemoving"]:Disconnect()
+					Connections["ESP_PlayerRemoving"] = nil
+				end
+				
+				ESP.Stop()
+			end
+		end
+	})
+
+	CombatGroupBox:AddToggle('AutoDodge', {
+		Text = 'AutoDodge',
+		Default = false, 
+		Tooltip = 'Whether or not the character should auto dodge when an enemy feints an attack.',
+
+		Callback = function(Value)
+			print('[cb] MyToggle changed to:', Value)
+		end
+	})
+	
+	CombatGroupBox:AddSlider('AutoParryDelay', {
+		Text = 'AutoParry Delay',
+		Default = 0.5,
+		Min = 0,
+		Max = 1,
+		Suffix = "s",
+		Rounding = 1,
+		Compact = true,
+
+		Callback = function(Value)
+			print('[cb] AutoDodge Delay was changed! New value:', Value)
+		end
+	})
+	
+	CombatGroupBox:AddSlider('AutoDodgeDelay', {
+		Text = 'AutoDodge Delay',
+		Default = 0.25,
+		Min = 0,
+		Max = 1,
+		Suffix = "s",
+		Rounding = 1,
+		Compact = true,
+
+		Callback = function(Value)
+			print('[cb] AutoDodge Delay was changed! New value:', Value)
+		end
+	})
+	
+	Toggles.AutoParry:OnChanged(function()
+		print('AutoParry changed to:', Toggles.AutoParry.Value)
+	end)
+	
+	Toggles.AutoDodge:OnChanged(function()
+		print('AutoDodge changed to:', Toggles.AutoDodge.Value)
+	end)
+
+	Library:OnUnload(function()
+		print('Unloaded!')
+		Library.Unloaded = true
+	end)
+
+	MenuGroupBox:AddButton('Unload', function()
+		Library:Unload()
+	end)
+	
+	ThemeManager:SetLibrary(Library)
+	
+	SaveManager:IgnoreThemeSettings()
+	ThemeManager:ApplyToTab(Tabs.Settings)
+	SaveManager:LoadAutoloadConfig()
+end
+
+
 
 return Utils
